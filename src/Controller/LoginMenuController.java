b274e2eb350d85c57649d50d200056ec994617e6
PoriaKH <poria.gm@gmail.com>
2022-04-19 18:43:47 +0430

    added userCreateRegex and getMatcher

--- src/Controller/LoginMenuController.java
+++ src/Controller/LoginMenuController.java
@@ -1,19 +1,21 @@
 package Controller;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginMenuController {
     public Matcher getMatcher(String regex,String command){
-        Matcher matcher;
-
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(command);
 
         return matcher;
     }
     public boolean userExist(String command){
 
     }
-    public String createUser(String command){
-
+    public String createUser(String command, String regex){
+        Matcher matcher = getMatcher(command,regex);
+        matcher.find();
     }
     public String login(String command){
 
--- src/View/LoginMenu.java
+++ src/View/LoginMenu.java
@@ -13,16 +13,38 @@ public class LoginMenu {
 
         LoginMenuController loginMenuController = new LoginMenuController();
 
-
+        String showCurrentMenuRegex = "";
+        String userCreateRegex1 = "(user create --username )(?<username>.*) --nickname (?<nickname>.*) --password (?<password>.*)";
+        String userCreateRegex2 = "(user create --username )(?<username>.*) --password (?<password>.*) --nickname (?<nickname>.*)";
+        String userCreateRegex3 = "(user create --nickname )(?<nickname>.*) --username (?<username>.*) --password (?<password>.*)";
+        String userCreateRegex4 = "(user create --nickname )(?<nickname>.*) --password (?<password>.*) --username (?<username>.*)";
+        String userCreateRegex5 = "(user create --password )(?<password>.*) --nickname (?<nickname>.*) --username (?<username>.*)";
+        String userCreateRegex6 = "(user create --password )(?<password>.*) --username (?<username>.*) --nickname (?<nickname>.*)";
+        String userCreateRegex7 = "(user create -u )(?<username>.*) -n (?<nickname>.*) -p (?<password>.*)";
         String loginRegex = "";
+        String enterMainMenuRegex = "";
 
         String command;
         command = scan.nextLine();
 
         while(!Objects.equals(command, "menu exit")){
-
             //TODO... command.matches Call the LoginMenuController Functions
-
+            if(command.matches(userCreateRegex1) || command.matches(userCreateRegex2) || command.matches(userCreateRegex3) || command.matches(userCreateRegex4) || command.matches(userCreateRegex5) || command.matches(userCreateRegex6) || command.matches(userCreateRegex7)){
+                if(command.matches(userCreateRegex1))
+                    System.out.println(loginMenuController.createUser(command,userCreateRegex1));
+                else if(command.matches(userCreateRegex2))
+                    System.out.println(loginMenuController.createUser(command, userCreateRegex2));
+                else if(command.matches(userCreateRegex3))
+                    System.out.println(loginMenuController.createUser(command, userCreateRegex3));
+                else if(command.matches(userCreateRegex4))
+                    System.out.println(loginMenuController.createUser(command, userCreateRegex4));
+                else if(command.matches(userCreateRegex5))
+                    System.out.println(loginMenuController.createUser(command, userCreateRegex5));
+                else if(command.matches(userCreateRegex6))
+                    System.out.println(loginMenuController.createUser(command, userCreateRegex6));
+                else if(command.matches(userCreateRegex7))
+                    System.out.println(loginMenuController.createUser(command, userCreateRegex7));
+            }
 
             command = scan.nextLine();
         }
