1b93110bcdad6cc2d7804d6c482b2ecc39efb853
amirkoochk <amirkoochk@gmail.com>
2022-07-20 13:32:15 +0430

    debug

--- server/src/main/java/Controller/PlayGameMenuController.java
+++ server/src/main/java/Controller/PlayGameMenuController.java
@@ -14,6 +14,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.regex.Matcher;
 
